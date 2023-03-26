3b43ddf2b201c910e8e5eefc4e3eeb0a55c6a071
hayksakulyan <sakulyanhayk@gmail.com>
2023-03-26 14:53:01 +0400

    "Added project files"

---
+++ Easy Lunch/Splash/View/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Lunch
+//
+//  Created by Hayk Sakulyan on 26.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
